0755f31928242ee6ecca63f8caa1441489ffec69
NoeSamaille <noe.samaille@protonmail.com>
2023-06-08 23:02:17 +0200

    Adds API
    Signed-off-by: NoeSamaille <noe.samaille@protonmail.com>

--- server.py
+++ server.py
@@ -1,10 +1,22 @@
 from flask import Flask
+import requests
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.get('/')
 def hello():
     return 'Hello Flow'
+
+@app.get('/quote')
+def quote():
+    category = 'computers'
+    api_url = 'https://api.api-ninjas.com/v1/quotes?category={}'.format(category)
+    response = requests.get(api_url, headers={'X-Api-Key': os.environ['API_KEY']})
+    if response.status_code == requests.codes.ok:
+        return response.text
+    else:
+        print("Error:", response.status_code, response.text)
 
 if __name__ == '__main__':
     app.run(port=5000, host="0.0.0.0")
